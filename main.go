9bebabf379f25b0d9c9f9b2bf1e7d40aa6196cac
Wordluc <40872910+Wordluc@users.noreply.github.com>
2024-04-19 22:17:45 +0200

    feat: test ricerca dicotomica

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"FLanguage/Evaluator"
 	Lexer "FLanguage/Lexer"
+	"FLanguage/Lexer/Token"
+	"FLanguage/Parser/Statements"
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func lexerFromFile() {
-	file, _ := Lexer.GetByteFromFile("prova.txt")
-	lexer, _ := Lexer.New(file)
-	for {
-		v, e := lexer.NextToken()
-		if e != nil {
-			break
-		}
-		fmt.Println(v)
-	}
+func lexerFromFile(path string) (Lexer.Lexer, error) {
+	file, _ := Lexer.GetByteFromFile(path)
+	lexer, e := Lexer.New(file)
+	return lexer, e
 }
 func main() {
+	reder := bufio.NewReader(os.Stdin)
+	fmt.Print("-")
+	v, _ := reder.ReadBytes('\n')
+	v = v[:len(v)-2]
+	l, e := lexerFromFile(string(v))
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
 
+	p, e := Statements.ParsingStatement(&l, Token.END)
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+	println("-----INIZIO-----")
+	env := Evaluator.NewEnvironment()
+	Evaluator.LoadBuiltInFunction(env)
+	Evaluator.LoadBuiltInVariable(env)
+	_, e = Evaluator.Eval(p.(*Statements.StatementNode), env)
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+	fmt.Println("-----FINE-----")
+	return
 }
