7e1c644f047c677a29581633d8a2e6aaf0aa3369
Wordluc <40872910+Wordluc@users.noreply.github.com>
2024-05-01 15:46:20 +0200

    fix

--- Parser/ExpresionLeaf.go
+++ Parser/ExpresionLeaf.go
@@ -9,8 +9,14 @@ func parseLeaf(l *Lexer.Lexer, _ IExpresion, exitTokens ...Token.TokenType) (IEx
 	curToken := l.LookCurrent()
 	leaf := ExpresionLeaf{}.New(curToken)
 
-	if nextToken, _ := l.NextToken(); nextToken.Type == Token.OPEN_CIRCLE_BRACKET {
+	if nextToken, _ := l.LookNext(); nextToken.Type == Token.OPEN_CIRCLE_BRACKET {
+		l.IncrP()
 		return parseCircleBracket(l, leaf, exitTokens...)
 	}
+	if nextToken, _ := l.LookNext(); nextToken.Type == Token.OPEN_GRAP_BRACKET {
+		l.IncrP()
+		return parseHash(l, leaf, exitTokens...)
+	}
+	l.IncrP()
 	return leaf, nil
 }
